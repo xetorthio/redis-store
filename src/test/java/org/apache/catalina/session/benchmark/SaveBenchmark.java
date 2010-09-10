fc9937d8307da45d20ea508dce43c4f2c773c1ea
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2010-09-10 08:47:01 -0300

    Added benchmark tests

---
+++ src/test/java/org/apache/catalina/session/benchmark/SaveBenchmark.java
@@ -0,0 +1,42 @@
+package org.apache.catalina.session.benchmark;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import org.apache.catalina.Session;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.session.PersistentManager;
+import org.apache.catalina.session.RedisStore;
+
+import redis.clients.jedis.Protocol;
+
+public class SaveBenchmark {
+    private static final int TOTAL_OPERATIONS = 10000;
+
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+	RedisStore.setDatabase(0);
+	RedisStore.setHost("localhost");
+	RedisStore.setPassword("foobared");
+	RedisStore.setPort(Protocol.DEFAULT_PORT);
+
+	PersistentManager manager = new PersistentManager();
+	manager.setContainer(new StandardContext());
+	RedisStore rs = new RedisStore();
+	rs.setManager(manager);
+
+	long begin = Calendar.getInstance().getTimeInMillis();
+	for (int n = 0; n < TOTAL_OPERATIONS; n++) {
+	    Session session = manager.createSession(null);
+	    rs.save(session);
+	}
+	long ellapsed = Calendar.getInstance().getTimeInMillis() - begin;
+
+	System.out.println((1000 * TOTAL_OPERATIONS / ellapsed)
+		+ " saves / second");
+    }
+
+}
